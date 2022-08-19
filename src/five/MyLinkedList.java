71591524f570c9c07adf9419613073ddff599d0f
ducmtgch200681 <ducmtgch200681@fpt.edu.vn>
2022-08-19 12:01:31 +0700

    oko

--- src/five/MyLinkedList.java
+++ src/five/MyLinkedList.java
@@ -1,24 +1,36 @@
 package five;
 
-public class MyLinkedList
+public class MyLinkedList<T>
 {
+    private class Node
+    {
+        public T data;
+        public Node next;
+
+        public Node(T data)
+        {
+            this.data = data;
+            this.next = null;
+        }
+    }
+
     private Node head = null;
     private Node tail = null;
 
     // Add a new node into the end of LinkedList
-    public void add(int data)
+    public void add(T data)
     {
         // 1. Create a new node
         Node one = new Node(data);
 
         // 2. Connect the new node 'one' to the end of LinkedList
-        // 2.1. There is currently to node
+        // 2.1. There is currently no node
         if(head == null) {
             head = one;
             tail = one;
         }
 
-        // 2.2. There are currently come nodes in the LinkedList
+        // 2.2. There are currently some nodes in the LinkedList
         else {
             // 2.2.1. Go to the last node
 //            Node current = head;
@@ -34,7 +46,7 @@ public class MyLinkedList
         }
     }
 
-    public void addFirst(int data)
+    public void addFirst(T data)
     {
         Node one = new Node(data);
 
@@ -42,52 +54,122 @@ public class MyLinkedList
         head = one;
     }
 
-    public void removeFirst()
+    public T removeFirst()
     {
         if(head != null)
         {
             Node temp = head;
+            T x = (T) temp.data;
             head = head.next;
             temp.next = null;
+
+            return x;
+        }
+        else return null;
+    }
+
+    public T removeLast()
+    {
+        if(head==null) return null;
+        else {
+            if(head.next == null)
+            {
+                T x = (T) head.data;
+                head = tail = null;
+            }
+            Node current = head;
+            Node temp = head;
+            while (current.next != null) {
+                temp = current;
+                current = current.next;
+            }
+            T x = (T) current.data;
+            temp.next = null;
+            tail = temp;
+            return x;
         }
     }
 
-    public void removeLast()
+    public void addMiddle(int index, T data)
     {
-        Node current = head;
-        Node temp = head;
-        while(current.next != null)
-        {
-            temp = current;
-            current = current.next;
+        if(index < 0) return;
+        else if (index == 0) addFirst(data);
+        else {
+            // 1. Create a new node
+            Node one = new Node(data);
+            // 2. Move to the node at position: index-1
+            Node current = head;
+
+            int count = 0;
+            while (current.next != null && count < index - 1) {
+                current = current.next;
+                count++;
+            }
+            if(count == index -1 && current.next != null)
+            {
+                // 3. Change the connections
+                one.next = current.next;
+                current.next = one;
+            }
+            else return;
+
         }
-        temp.next = null;
-        tail = temp;
     }
 
-    public void add(int index, int data)
+    public void removeMiddle(int index)
     {
+        if(index < 0) return;
+        else if (index == 0) removeFirst();
+        else {
+            Node current = head;
 
-    }
+            int count = 0;
+            while (current.next != null && count < index - 1) {
+                current = current.next;
+                count++;
+            }
+            if(count == index -1 && current.next != null)
+            {
+                Node temp = current.next;
+                current.next = temp.next;
+                temp.next = null;
+            }
+            else return;
 
-    public void remove(int index, int data)
-    {
-
+        }
     }
 
     public int contains(int data)
     {
+        Node current = head;
+
+        if(current.data.equals(data))
+        {
+
+        }
         return -1;
     }
 
-    public void print()
+    public void print(boolean h)
     {
-        Node current = head;
-        while (current != null)
-        {
-            System.out.println(current.data + " ");
-            current = current.next;
+        if(h == true){
+            Node current = head;
+            while (current != null)
+            {
+                System.out.println(current.data + " ");
+                current = current.next;
+            }
+            System.out.println();
         }
-        System.out.println();
+        if(h == false)
+        {
+            Node current = head;
+            while (current != null)
+            {
+                System.out.println(removeLast() + " ");
+                current = current.next;
+            }
+            System.out.println();
+        }
     }
 }
--- src/five/Test.java
+++ src/five/Test.java
@@ -4,25 +4,27 @@ public class Test
 {
     public static void main (String[] args)
     {
-        MyLinkedList MyList = new MyLinkedList();
+        MyLinkedList<Integer> MyList = new MyLinkedList<>();
         MyList.add(9);
         MyList.add(10);
         MyList.add(5);
         MyList.add(8);
-        MyList.addFirst(7);
-        MyList.removeFirst();
-        MyList.removeLast();
-        MyList.print();
+//        MyList.addFirst(7);
+//        MyList.removeFirst();
+//        MyList.removeLast();
+//        MyList.addMiddle(4,11);
+//        MyList.removeMiddle(3);
+        MyList.print(false);
 
-        Node one = new Node(9);
-        Node two = new Node(10);
-        Node three = new Node(5);
-        Node four = new Node(8);
+//        Node one = new Node(9);
+//        Node two = new Node(10);
+//        Node three = new Node(5);
+//        Node four = new Node(8);
 
         //link node
-        one.next = two;
-        two.next = three;
-        three.next = four;
+//        one.next = two;
+//        two.next = three;
+//        three.next = four;
 
         //Test
 //        System.out.println(three.data);
