e7cd8537841e1f79a15fbd4dd2a0a366112d02ea
ducmtgch200681 <ducmtgch200681@fpt.edu.vn>
2022-09-07 16:26:19 +0700

    ok

--- src/five/SinglyLinkedList.java
+++ src/five/SinglyLinkedList.java
@@ -161,17 +161,6 @@ public class SinglyLinkedList<T>
         }
     }
 
-//    public int contains(int data)
-//    {
-//        Node current = head;
-//
-//        if(current.data.equals(data))
-//        {
-//
-//        }
-//        return -1;
-//    }
-
     public void print(boolean flag)
     {
         p(head, flag);
@@ -190,95 +179,95 @@ public class SinglyLinkedList<T>
         }
     }
 
-    public int length()
-    {
-        int count = 0;
-        Node current = head;
-        while (current.next != null) {
-            current = current.next;
-            count++;
-        }
-        return count + 1;
-    }
-
-    public int[] toArray(SinglyLinkedList<T> List)
-    {
-        int n = List.length();
-        int[] result = new int[n];
-        int i = 0;
-        Node current = head;
-
-        while (current.next != null)
-        {
-            result[i++] = (int) current.data;
-            current = current.next;
-        }
-        result[i++] = (int) current.data;
-
-        return result;
-    }
-
-
-    // Java program for implementation of Heap Sort
-
-    public void sort(SinglyLinkedList<T> List)
-    {
-        int n = List.length();
-        int[] arr = List.toArray(List);
-
-        // Build heap (rearrange array)
-        for (int i = n / 2 - 1; i >= 0; i--){
-            heapify(arr, n, i);
-        }
-
-        // One by one extract an element from heap
-        for (int i = n - 1; i > 0; i--) {
-            // Move current root to end
-            int temp = arr[0];
-            arr[0] = arr[i];
-            arr[i] = temp;
-            // call max heapify on the reduced heap
-            heapify(arr, i, 0);
-        }
-
-        printArray(arr, List);
-    }
-
-    // To heapify a subtree rooted with node i which is
-    // an index in arr[]. n is size of heap
-    void heapify(int[] arr, int n, int i)
-    {
-        int largest = i; // Initialize largest as root
-        int l = 2 * i + 1; // left = 2*i + 1
-        int r = 2 * i + 2; // right = 2*i + 2
-
-        // If left child is larger than root
-        if (l < n && arr[l] > arr[largest])
-            largest = l;
-
-        // If right child is larger than largest so far
-        if (r < n && arr[r]> arr[largest])
-            largest = r;
-
-        // If largest is not root
-        if (largest != i) {
-            int swap = arr[i];
-            arr[i] = arr[largest];
-            arr[largest] = swap;
-
-            // Recursively heapify the affected sub-tree
-            heapify(arr, n, largest);
-        }
-    }
-
-    /* A utility function to print array of size n */
-    void printArray(int[] arr, SinglyLinkedList<T> List)
-    {
-        int n = List.length();
-        for (int i = 0; i < n; ++i)
-            System.out.print(arr[i] + " ");
-        System.out.println();
-    }
+//    public int length()
+//    {
+//        int count = 0;
+//        Node current = head;
+//        while (current.next != null) {
+//            current = current.next;
+//            count++;
+//        }
+//        return count + 1;
+//    }
+//
+//    public int[] toArray(SinglyLinkedList<T> List)
+//    {
+//        int n = List.length();
+//        int[] result = new int[n];
+//        int i = 0;
+//        Node current = head;
+//
+//        while (current.next != null)
+//        {
+//            result[i++] = (int) current.data;
+//            current = current.next;
+//        }
+//        result[i++] = (int) current.data;
+//
+//        return result;
+//    }
+//
+//
+//    // Java program for implementation of Heap Sort
+//
+//    public void sort(SinglyLinkedList<T> List)
+//    {
+//        int n = List.length();
+//        int[] arr = List.toArray(List);
+//
+//        // Build heap (rearrange array)
+//        for (int i = n / 2 - 1; i >= 0; i--){
+//            heapify(arr, n, i);
+//        }
+//
+//        // One by one extract an element from heap
+//        for (int i = n - 1; i > 0; i--) {
+//            // Move current root to end
+//            int temp = arr[0];
+//            arr[0] = arr[i];
+//            arr[i] = temp;
+//            // call max heapify on the reduced heap
+//            heapify(arr, i, 0);
+//        }
+//
+//        printArray(arr, List);
+//    }
+//
+//    // To heapify a subtree rooted with node i which is
+//    // an index in arr[]. n is size of heap
+//    void heapify(int[] arr, int n, int i)
+//    {
+//        int largest = i; // Initialize largest as root
+//        int l = 2 * i + 1; // left = 2*i + 1
+//        int r = 2 * i + 2; // right = 2*i + 2
+//
+//        // If left child is larger than root
+//        if (l < n && arr[l] > arr[largest])
+//            largest = l;
+//
+//        // If right child is larger than largest so far
+//        if (r < n && arr[r]> arr[largest])
+//            largest = r;
+//
+//        // If largest is not root
+//        if (largest != i) {
+//            int swap = arr[i];
+//            arr[i] = arr[largest];
+//            arr[largest] = swap;
+//
+//            // Recursively heapify the affected sub-tree
+//            heapify(arr, n, largest);
+//        }
+//    }
+//
+//    /* A utility function to print array of size n */
+//    void printArray(int[] arr, SinglyLinkedList<T> List)
+//    {
+//        int n = List.length();
+//        for (int i = 0; i < n; ++i)
+//            System.out.print(arr[i] + " ");
+//        System.out.println();
+//    }
 
     Node sortedMerge(Node a, Node b)
     {
